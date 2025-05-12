f74ef013f89061ae34f2748588f9bdfaaa898239
FRISKSYA <frics5090fksk@gmail.com>
2025-05-12 18:52:12 +0900

    setting 画面固定

--- mobileModule03/advanced_weather_app/lib/main.dart
+++ mobileModule03/advanced_weather_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'core/constants/app_constants.dart';
 import 'core/theme/app_theme.dart';
 import 'core/widgets/app_background.dart';
 import 'screens/home/home_screen.dart';
 
 void main() {
-  runApp(const WeatherApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  // 画面を縦向きに固定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const WeatherApp());
+  });
 }
 
 class WeatherApp extends StatelessWidget {
