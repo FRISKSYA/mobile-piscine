a8593776a182bdad84b65aea150bc643be151d84
FRISKSYA <frics5090fksk@gmail.com>
2025-07-25 18:49:19 +0900

    reset

--- mobileModule04/diary_app/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Basic app test', (WidgetTester tester) async {
-    // TODO: Add proper widget tests after setting up test environment
-    expect(true, true);
-  });
-}
